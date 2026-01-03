1eab53a79f47e1db9aa282446c8bfd832659b210
Ivriel <gunawanivriel@gmail.com>
2026-01-03 22:33:40 +0700

    Has one of many

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
 use App\Models\Product;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\ProductSeeder;
@@ -14,9 +15,12 @@ class ProductTest extends TestCase
     /**
      * A basic feature test example.
      */
+    public function seeder(){
+        $this->seed([CategorySeeder::class,ProductSeeder::class]);
+    }
     public function testOneToMany(): void
     {
-        $this->seed([CategorySeeder::class,ProductSeeder::class]);
+        $this->seeder();
 
         $product = Product::find('1');
         self::assertNotNull($product);
@@ -24,5 +28,20 @@ class ProductTest extends TestCase
         $category = $product->category;
         self::assertNotNull($category);
         self::assertEquals("FOOD",$category->id);
+    }
+
+    public function testHasOneOfMany()
+    {
+        $this->seeder();
+        $category = Category::find("FOOD");
+        self::assertNotNull($category);
+
+        $cheapestProduct = $category->cheapestProduct;
+        self::assertNotNull($cheapestProduct);
+        self::assertEquals("1",$cheapestProduct->id);
+
+        $mostExpensiveProduct = $category->mostExpensiveProduct;
+        self::assertNotNull($mostExpensiveProduct);
+        self::assertEquals("2",$mostExpensiveProduct->id);
     }
 }
