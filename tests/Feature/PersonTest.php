c67e2ad5fe74517e4f938ab6f3bef83be5aea22b
Ivriel <gunawanivriel@gmail.com>
2026-01-25 20:08:41 +0700

    Add test for attribute casting with Carbon datetime instances

--- tests/Feature/PersonTest.php
+++ tests/Feature/PersonTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Person;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,10 +19,25 @@ class PersonTest extends TestCase
         $person->first_name = "Ivriel";
         $person->last_name = "Gunawan";
         $person->save();
+
         self::assertEquals("IVRIEL Gunawan",$person->full_name);
         $person->full_name = "Ivriel Dei";
         $person->save();
+
         self::assertEquals("IVRIEL",$person->first_name);
         self::assertEquals("Dei",$person->last_name);
+    }
+     public function testAttributeCasting()
+    {
+        $person = new Person();
+        $person->first_name = "Ivriel";
+        $person->last_name = "Gunawan";
+        $person->save();
+
+        self::assertNotNull($person->created_at);
+        self::assertNotNull($person->updated_at);
+
+        self::assertInstanceOf(Carbon::class,$person->created_at);
+        self::assertInstanceOf(Carbon::class,$person->updated_at);
     }
 }
